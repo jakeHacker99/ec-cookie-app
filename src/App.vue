0d6d2c849864fa61f74adfa75bbb7a8c94000966
jakob <jakobyaro99@gmail.com>
2020-12-16 15:50:43 +0100

    ec-cookie-app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="" class="flex-container center ">
+    <img alt="Vue logo" src="./assets/logo.png" style="width: 20%;" class="center">
+
+    <CookieConsent/>
+
+
+    <h1 class="center" style="font-size: 2rem;">Övning Cookies</h1>
+    <h3 class="center" style="font-size:.7rem;">Här har jag bygg en cookie, fick du upp ett jobbig 
+      meddelande så vet du att den funkar</h3>
   </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CookieConsent from './components/CookieConsent'
+
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    CookieConsent
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+.flex-container {
+  display: flex;
+  display: row;
 }
+
+.center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+  color: blueviolet;
+
+}
+
 </style>
---
+++ src/components/CookieConsent.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="cookie-container"  >
+    <p>
+      the Detecify website uses cookie to make your online experience easier and
+      better. By using our website, you consent to our use of cookies. For more
+      information, see our <a href="#">privacy policy</a>
+    </p>
+
+    <button class="cookie-btn" @click="agree" >agree</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  
+  data() {return {
+      consented:false,
+    }},
+    methods:{
+      agree(){
+        localStorage.setItem("cookieConsented", "true")
+        this.consented= true
+        
+      }
+    },
+    created(){
+      if(localStorage.getItem("cookieConsented")){
+        this.consented =true
+      }
+    }
+};
+</script>
+
+
+<style scoped>
+.cookie-container {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: #2f3;
+  color: black;
+  padding: 0 32px;
+  box-shadow: 0 -2px 16px rgba(47, 54, 64);
+
+}
+
+.cookie-container a {
+  color: black;
+}
+
+.cookie-btn {
+  background: blueviolet;
+  border: 0;
+  color: black;
+  padding: 12px 48px;
+  font-size: 18px;
+  margin-bottom: 16px;
+  border-radius: 8px;
+  cursor: pointer;
+}
+</style>
